a9d5b4636b56b90d2158f5406cf5975fbaaeec91
Nikita <nikitajaiswal23@gmail.com>
2022-06-04 19:27:48 +0530

    Add project files.

---
+++ MvcDemoAPI/Migrations/20220520103835_Ontu.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MvcDemoAPI.Migrations
+{
+    public partial class Ontu : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
